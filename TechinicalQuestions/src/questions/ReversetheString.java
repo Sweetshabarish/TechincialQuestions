3ccdb4cd423b23aba392300bfe333cdded09e30b
Sweet Shabarish <70278432+Sweetshabarish@users.noreply.github.com>
2022-10-03 17:29:43 +0530

    Update ReversetheString.java

--- TechinicalQuestions/src/questions/ReversetheString.java
+++ TechinicalQuestions/src/questions/ReversetheString.java
@@ -8,15 +8,22 @@ import java.util.Scanner;
  * 1. Write a program to reverse a given number
  */
 public class ReversetheString {
+	public static String Rversestring(String stringvalue) {
+		  String reverse = "";
+		  int length = stringvalue.length();
+		  while (length > 0) {
+		    reverse += stringvalue.charAt(length - 1);
+		    length--;
+		  }
+		  return reverse;
+		}
 	public static void main(String[] args) {
 		
 		Scanner inputreader=new Scanner(System.in);//assiging the inputreader as user input
-		String inputstring=inputreader.next();//geting value from the user
-		String reversestring="";//creating dummy string ,to store the reverse value
-		for(int index=inputstring.length()-1;index>=0;index--) {//using for loop in reverse manner
-			reversestring+=inputstring.charAt(index);//storing the single character in reverse manner in the reverse string
-		}
-		System.out.println(reversestring);// displaying the character
+		String inputstring=inputreader.nextLine();//getting a string from the user
+		
+	     
+		System.out.println(Rversestring(inputstring));
 	}
 
 }
