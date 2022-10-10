e8a2f0142d7778b2c12b7f4d22494291100178d2
Sweet Shabarish <70278432+Sweetshabarish@users.noreply.github.com>
2022-10-10 15:52:00 +0530

    Update Reversethenumber.java

--- TechinicalQuestions/src/questions/Reversethenumber.java
+++ TechinicalQuestions/src/questions/Reversethenumber.java
@@ -3,22 +3,26 @@ package questions;
 import java.util.Scanner;
 
 public class Reversethenumber {
-	public static String Rversenumber(String stringvalue) {
-		  String reversenumber = "";
-		  int length = stringvalue.length();
-		  while (length > 0) {
-		    reversenumber += stringvalue.charAt(length - 1);
-		    length--;
-		  }
-		  return reversenumber;
-		}
+	public static void Rversenumber(int number) {
+
+        if (number <= 0) {//if the number contains less than zero means it will return present number
+            
+            Rversenumber(number );
+        } else if (number < 10) {//if number less than 10 means it will return single digigt number
+            System.out.println(number);
+        }
+        else {
+            System.out.print(number % 10);//printing number in the reverse module
+            Rversenumber(number / 10);
+        }
+    }
 	public static void main(String[] args) {
 		
 		Scanner inputreader=new Scanner(System.in);//assiging the inputreader as user input
-		String inputstring=inputreader.nextLine();//getting a number as a string from the user
+		int inputnumber=10000;//getting a number as a string from the user
 		
 	     
-		System.out.println(Rversenumber(inputstring));
+		Rversenumber(inputnumber);//sending the input number to the Reverse number method
 	}
 
 }
